51ef07e07399eef685b17bbf76416e14a03869d4
iamkaizen <kimsokhom654@gmail.com>
2024-09-27 15:37:04 +0700

    fix clearHeatmap function

--- modules/ui.js
+++ modules/ui.js
@@ -1,4 +1,4 @@
-//This module manages the UI logic and interaction
+// This module manages the UI logic and interaction
 
 export function initUI() {
     // Initialize UI setup if needed
@@ -6,15 +6,15 @@ export function initUI() {
 
 // Function to update the click history in the UI
 export function updateClickHistory(clickedLayers) {
-    window.parent.postMessage({ type: 'update-click-history', clickedLayers }, '*');
+    figma.ui.postMessage({ type: 'update-click-history', clickedLayers });
 }
 
 // Function to display the heatmap in the UI
 export function displayHeatmap(heatmapData) {
-    window.parent.postMessage({ type: 'display-heatmap', heatmapData }, '*');
+    figma.ui.postMessage({ type: 'display-heatmap', heatmapData });
 }
 
 // Function to clear the heatmap in the UI
 export function clearHeatmap() {
-    window.parent.postMessage({ type: 'clear-heatmap' }, '*');
+    figma.ui.postMessage({ type: 'clear-heatmap' });
 }
